1b297382fe211b008fa34595764359eba7431945
onkar31 <onkarbhogle08@gmail.com>
2026-07-09 11:05:28 +0530

    Array: Right rotate element by K

--- src/main/java/Arrays/Easy/RotateArrayBy_K.java
+++ src/main/java/Arrays/Easy/RotateArrayBy_K.java
@@ -2,24 +2,37 @@ package Arrays.Easy;
 
 //Given an array of integers, rotating array of elements by k elements either left or right.
 
-import java.util.Arrays;
-
 public class RotateArrayBy_K {
 
     public static void main(String[] args) {
-
+        int[] arr = {1, 2, 3, 4, 5};
+        int k= 6;
+        int[] result = rotateRight(arr, k);
+        for(int num : result){
+            System.out.print(num + " ");
+        }
     }
 
     // Rotate the array to the right by k positions
-    public static void rotateRight(int[] arr, int k) {
-        int n = arr.length;
-        if(n==0) return;
+    public static int[] rotateRight(int[] nums, int k) {
+        int n = nums.length;
+        if(n==0) return nums;
 
         k = k % n;
-        //store last k elements
-        int[] temp = Arrays.copyOfRange(arr, n-k, n);
+        reverse(nums, 0, nums.length -1);
+        reverse(nums, 0, k -1);
+        reverse(nums, k, nums.length -1);
+        return nums;
+    }
 
-        // Shift the remaining elements to the right
-
+    public static int[] reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+        return nums;
     }
 }
